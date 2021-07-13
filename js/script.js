6f94c834daad395a518fee7713bb0a155c4de38d
miseryangel <miseryangel1990@gmail.com>
2021-07-13 17:25:20 -0400

    one more time

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
       var propRandomValue = "{{" + chosenCategoryShortName + "}}";
-      var randomPage = insertProperty(homeHtml,"name",propRandomValue);
+      var randomPage = insertProperty(homeHtml,"randomCategoryShortName",propRandomValue);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
